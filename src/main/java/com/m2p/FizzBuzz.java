6e4d46743eea9a8c56aca3589342aef198c1bef3
VarshaaSK <varshaa.sk@m2pfintech.com>
2023-03-29 22:59:52 +0530

    Add. Included the Logic to return FizzBuzz

--- src/main/java/com/m2p/FizzBuzz.java
+++ src/main/java/com/m2p/FizzBuzz.java
@@ -1,4 +1,17 @@
 package com.m2p;
 
 public class FizzBuzz {
+    
+    private int number;
+    FizzBuzz(int number){
+        this.number = number;
+    }
+    public String multipleOfThreeAndFive() {
+        if(number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        }
+        return "Does not Match";
+    }
+    
+    
 }
--- src/test/java/com/m2p/FizzBuzzTest.java
+++ src/test/java/com/m2p/FizzBuzzTest.java
@@ -1,5 +1,24 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
-    FizzBuzz fizzbuzz = new FizzBuzz();
+    FizzBuzz fizzbuzz = new FizzBuzz(15);
+
+    @Test
+    void toReturnFizBuzz(){
+
+        //Arrange
+        String expectedString = "FizzBuzz";
+
+        //Act
+        String actualString = fizzbuzz.multipleOfThreeAndFive();
+
+        //Assert
+        assertEquals(expectedString , actualString);
+    }
+
+
 }
